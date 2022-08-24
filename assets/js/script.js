7dcf1d1f9fc688f3a9541a576df1d3d6cd6a01eb
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-08-24 14:13:19 -0400

    API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-var search = document.querySelector(".searchContainer");
-var main = document.querySelector(".mainContianer");
-var city = document.querySelector(".city");
-var temp = document.querySelector(".temp");
-var wind = document.querySelector(".wind");
-var huminity= document.querySelector(".huminity");
-var uv = document.querySelector(".uv");
-var forecast = document.querySelector(".fiveDay");
-
-var apiKey="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
-
-var APIurl="https://api.openweathermap.org/data";
-
+var apiKey="9e9ea344324232c8d60d73d284628773";
+var currentCity ="";
+var previousCity = "";
